eb7c39a20110ccbcc16a91d2dd9cc39294e0d6df
jorgelucas2077 <jorgelucas2077@gmail.com>
2024-04-18 15:41:52 -0300

    JS

---
+++ JS/condição_verdadeiro_falso.js
@@ -0,0 +1,12 @@
+const idade = 18;
+const nome = 'Jorge';
+
+//if (idade >= 18 || nome === 'Jorge') {
+if (idade >= 18 && nome === 'Jorge') {
+    console.log('O usuario pode entrar')
+
+} else {
+    console.log('O usuario não é permitido entrar')
+}
+
+//console.log('O usuario pode passar', autorizado);
