647574689be40b201a02c9496935c77d291a4278
jorgelucas2077 <jorgelucas2077@gmail.com>
2024-05-09 01:46:56 -0300

    js

---
+++ JS/Fundamentos/operadores_aritmeticos.js
@@ -0,0 +1,29 @@
+var soma = 2+1;
+console.log("Soma:"+" "+soma); //soma
+
+var divisao = 2-1;
+console.log("Divisão:"+" "+divisao); //divisão
+
+var multiplicacao = 2*2;
+console.log("Multiplicação:"+" "+multiplicacao); //multiplicação
+
+var subtracao = 3/2;
+console.log("Subtração:"+" "+ subtracao); //subtração
+
+var restante = 10%3;
+console.log("Restante:" +" "+ restante); //modulo (resto)
+
+var elevacao = Math.pow(2,2); //elevação a potência
+console.log("Elevação:"+ " " +elevacao);
+
+var raiz = Math.sqrt(100); //Raiz quadrada
+console.log("Raiz:"+" "+ raiz);
+
+var arredondamento_inteiro = Math.ceil (5,3); //Arredondamento para cima
+console.log("Arrendodamento inteiro:" + " " + arredondamento_inteiro);
+
+var arrendodamento = Math.round (9,8); //Arredondamento proximo
+console.log("Arrendodamento:" + " " + arrendodamento);
+
+var arredondamento_baixo = Math.floor (9,5);
+console.log("Arrendodamento baixa" + " " + arredondamento_baixo);
