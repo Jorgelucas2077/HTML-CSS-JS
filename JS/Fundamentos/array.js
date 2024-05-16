97b958764329c24d31a104e745776f2e8a40e3f3
jorgelucas2077 <jorgelucas2077@gmail.com>
2024-05-16 03:32:31 -0300

    js

--- JS/Fundamentos/array.js
+++ JS/Fundamentos/array.js
@@ -6,5 +6,19 @@ for (var pos = 0; pos < valores.length; pos++){
     console.log("Posição: " + valores[pos]);
 }
 
-let text = "Hello World!";
-let length = text.length;
+var carros = [];
+carros[0] = "Volvo";
+carros[1] = "Jeep";
+
+var motos = new Array("BMW","Yamaha","Honda");
+
+// Calcular a média de todos os numeros de um array
+
+var soma = 0;
+for (var pos = 0; pos < valores.length; pos++){
+    soma += valores[pos];
+
+}
+
+var media = soma/valores.length;
+console.log(media);
