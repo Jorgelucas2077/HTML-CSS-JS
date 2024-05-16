d09a488ef6a7561889de0bd742a17cba26515448
jorgelucas2077 <jorgelucas2077@gmail.com>
2024-05-15 23:30:31 -0300

    js

---
+++ JS/Fundamentos/array.js
@@ -0,0 +1,10 @@
+var valores = [8,1,7,2,9];
+
+console.log(valores[2]);
+
+for (var pos = 0; pos < valores.length; pos++){
+    console.log("Posição: " + valores[pos]);
+}
+
+let text = "Hello World!";
+let length = text.length;
