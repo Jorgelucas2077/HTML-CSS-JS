613b18be22870f92cf6a2c36160718b9f7434711
Jorge Lucas <jorgelucas2077@gmail.com>
2024-05-20 02:52:49 -0300

    js

--- JS/Fundamentos/teste1.js
+++ JS/Fundamentos/teste1.js
@@ -1,14 +1,23 @@
-var matrizexemplo: [];
-var linhas = 4;
-var colunas = 3;
+var matrizexemplo = [
+];
+var linhas = 5;
+var colunas = 4;
 
 for (i =0; i < linhas; i++){
     matrizexemplo [i] = [];
 
+    var numeroBase = i + 2;
+
     for (j = 0; j < colunas; j++){
-        matrizexemplo [i][j] = j;
-    
+        
+        var multiplo = numeroBase * (j+1);
+        matrizexemplo [i][j] = multiplo;
     }
 }
+console.log (matrizexemplo);
+//var resultado = matrizexemplo.filter(item => item % 2 == 0);
+//console.log (resultado);
 
-console.log(matrizexemplo);
+//console.log(matrizexemplo);
+
+
